6ed4dd818cd90c1234ea5227fcad28a5846f0d4c
ADAS2024 <arnab.das00000@gmail.com>
2021-03-13 13:32:32 -0500

    Add new cards

--- app/src/main/java/com/example/arnabsflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/arnabsflashcardapp/MainActivity.java
@@ -13,15 +13,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     boolean isShowingAnswers = true;
-    FlashcardDatabase flashcardDatabase;
-    List<Flashcard> allFlashcards;
-    int currentCardDisplayedIndex = 0;
 
 
     @Override
@@ -37,45 +30,6 @@ public class MainActivity extends AppCompatActivity {
         TextView flashcardanswer3 = findViewById(R.id.flashcard_answer3);
         ImageView eyebutton =  (findViewById(R.id.toggle_choices_visibility));
         ImageView plusbutton = (findViewById(R.id.newcard));
-        ImageView nextbutton = (findViewById(R.id.nextbutton));
-
-
-        nextbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // don't try to go to next card if you have no cards to begin with
-                if (allFlashcards.size() == 0)
-                    return;
-                // advance our pointer index so we can show the next card
-                currentCardDisplayedIndex++;
-
-                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
-                if(currentCardDisplayedIndex >= allFlashcards.size()) {
-
-                    Snackbar.make(flashcardquestion,
-                            "You've reached the end of the cards, going back to start.",
-                            Snackbar.LENGTH_SHORT)
-                            .show();
-                    currentCardDisplayedIndex = 0;
-                }
-
-                // set the question and answer TextViews with data from the database
-                allFlashcards = flashcardDatabase.getAllCards();
-                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
-
-                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getAnswer());
-                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getQuestion());
-            }
-        });
-
-        flashcardDatabase = new FlashcardDatabase(this);
-        allFlashcards = flashcardDatabase.getAllCards();
-
-        if (allFlashcards != null && allFlashcards.size() > 0) {
-            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
-            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
-        }
-
 
 
 
@@ -88,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
+        
         flashcardquestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -168,9 +122,6 @@ public class MainActivity extends AppCompatActivity {
             flashcardquestion.setText(question);
             TextView flashcardanswer = findViewById(R.id.flashcard_answer);
             flashcardanswer.setText(answer);
-
-            flashcardDatabase.insertCard(new Flashcard(question, answer));
-            allFlashcards = flashcardDatabase.getAllCards();
         }
     }
 }
